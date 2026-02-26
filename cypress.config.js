374b051a404a637887649ed8e07fc9b387844a89
Marisleydi Hondar Martinez <mhondar@gmail.com>
2026-02-26 20:39:23 +0100

    chore: install and configure cypress base setup

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,19 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://demoqa.com',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
   },
+
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+
+  video: true,
+  screenshotOnRunFailure: true,
+  viewportWidth: 1280,
+  viewportHeight: 800,
+  defaultCommandTimeout: 8000,
 });
